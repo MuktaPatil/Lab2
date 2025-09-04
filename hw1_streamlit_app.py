021940decd863c8af8b21e840f6e7fa71839bb72
Mukta Patil <72442967+MuktaPatil@users.noreply.github.com>
2025-09-04 15:12:27 +0000

    ...

--- hw1_streamlit_app.py
+++ hw1_streamlit_app.py
@@ -27,7 +27,7 @@ st.title("📄 HW1: Document question answering")
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.secrets["openai"]['api_key']
+openai_api_key = st.secrets['openai']['api_key']
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
