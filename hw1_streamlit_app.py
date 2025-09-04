871714da1fbb22d7d7d598546339f4b9f0542560
Mukta Patil <72442967+MuktaPatil@users.noreply.github.com>
2025-09-04 15:10:42 +0000

    secrest and reqs

--- hw1_streamlit_app.py
+++ hw1_streamlit_app.py
@@ -22,15 +22,12 @@ def extract_text_from_txt(uploaded_file):
 
 # Show title and description.
 st.title("📄 HW1: Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
+
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.secrets["openai"]['api_key']
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
